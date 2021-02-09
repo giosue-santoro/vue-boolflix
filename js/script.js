0488305db97caed6cb194b4df24ba93b94eefc93
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-02-09 16:24:38 +0100

    rendering

--- js/script.js
+++ js/script.js
@@ -3,18 +3,22 @@ new Vue ({
     el: '#app',
 
     data: {
-        
+        movies: [],
+        inputValue: '',
     },
 
-    mounted() {
-		const self = this;
+    methods: {
+		
+        inputSearch: function() {
+            const self = this;
     
-        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-          .then((re) => {
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=6633b10121d740b0c55d03556425601b&query=' + self.inputValue)
+            .then((re) => {
+                self.movies = re.data.response
+            })
+            console.log(self.movies);
+        }
 
-            
-        })
-        
     }
 
 })

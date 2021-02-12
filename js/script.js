9527c58e25f55ef018e75ac7fa6ac2a3f7990dbe
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-02-12 17:40:19 +0100

    att

--- js/script.js
+++ js/script.js
@@ -6,9 +6,15 @@ new Vue ({
 
         movies: [],
         series: [],
+        actors: [],
         inputValue: '',
         showNavbar: true,
+        creditID: 0
+        
+    },
 
+    mounted() {
+        this.mountedSearch();
     },
 
     methods: {
@@ -21,12 +27,47 @@ new Vue ({
                 self.movies = re.data.results
             })
             console.log(self.movies);
+        },
+
+        secondInputSearch: function() {
+            const self = this;
 
             axios.get('https://api.themoviedb.org/3/search/tv?api_key=6633b10121d740b0c55d03556425601b&query=' + self.inputValue)
             .then((re) => {
                 self.series = re.data.results
             })
             console.log(self.series);
+        },
+
+        mountedMovies: function() {
+            const self = this;
+    
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=6633b10121d740b0c55d03556425601b&query=red')
+            .then((re) => {
+                self.movies = re.data.results
+            })
+            console.log(self.movies);
+        },
+
+        mountedTV: function() {
+            const self = this;
+
+            axios.get('https://api.themoviedb.org/3/search/tv?api_key=6633b10121d740b0c55d03556425601b&query=red')
+            .then((re) => {
+                self.series = re.data.results
+            })
+            console.log(self.series);
+        },
+
+        mountedSearch: function() {
+            this.mountedMovies();
+            this.mountedTV();
+        },
+
+        allSearch: function() {
+            this.inputSearch();
+            this.secondInputSearch();
+            console.log(this.actors);
         },
 
         getPoster: function(movie) {
@@ -64,6 +105,26 @@ new Vue ({
         starRating: function(el) {
             return Math.round(el / 2);
         },
+
+        // findActors: function () {
+        //     const self = this;
+        //     list = [];
+
+        //     axios
+        //         .get('https://api.themoviedb.org/3/movie/' + creditID + '/credits?api_key=6633b10121d740b0c55d03556425601b' )
+        //         .then(function(re) {
+        //           for (var i = 0; i < 5; x++) {
+        //             self.list += re.data.cast[i];
+        //         }
+        //     })
+
+        //     this.list = this.actors;
+        // },
+
+        // scrollRight: function() {
+        //     const target = document.getElementById('movie-container');
+        //     target.scrollLeft += 100;
+        // }
 
         // slideRight: function() {
         //     let content = document.getElementById('movie-container'),
